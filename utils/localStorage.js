e855a47334eedc75f458a6ba3b146c8c39f48d24
Guido Queiroz <Takzzg@hotmail.com>
2022-07-04 01:33:52 -0300

    old function removed

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -13,14 +13,3 @@ export const setLocalStorage = ({ contacts = [], groups = [] }) => {
     )
     window.localStorage.setItem("contacts-app-groups", JSON.stringify(groups))
 }
-
-export const deleteContact = (id) => {
-    let oldContacts = JSON.parse(
-        window.localStorage.getItem("contacts-app-contacts")
-    )
-    let newContacts = oldContacts.filter((u) => u.id !== id)
-    window.localStorage.setItem(
-        "contacts-app-contacts",
-        JSON.stringify(newContacts)
-    )
-}
